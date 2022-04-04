d3b333b74ab883977a78013f4f393750d7e71e6e
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2022-04-04 18:52:11 +0700

    [ADD] listview for stations and filtering station search

--- MainPage.py
+++ MainPage.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
+from PySide6.QtGui import *
 from main import Ui_Form as Ui_Main
 
 class Main_Page(QWidget):
@@ -11,13 +12,35 @@ class Main_Page(QWidget):
         self.ui = Ui_Main()
         self.ui.setupUi(self)
 
-        # self.ui.homeScrollArea.setWidget(self.ui.homePage)
-
-        # Pages
+        # Pages from stackedWidget
         self.ui.homePushButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.homePage))
         self.ui.historyPushButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.historyPage))
         self.ui.locationPushButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.locationPage))
         self.ui.accountPushButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.accountPage))
+
+        # Home Page
+        if (self.ui.stackedWidget.currentWidget() == self.ui.homePage):
+            self.ui.stackedWidget.currentWidget()
+
+            # Setting up stations for testing
+            self.stations=('Elex','Why','How','What','When','Aow','Im')
+            self.stationModel = QStandardItemModel(len(self.stations), 1)
+            self.stationModel.setHorizontalHeaderLabels(['Stations'])
+
+            for row, station in enumerate(self.stations):
+                item = QStandardItem(station)
+                self.stationModel.setItem(row, 0, item)
+            
+            self.filter_proxy_model = QSortFilterProxyModel()
+            self.filter_proxy_model.setSourceModel(self.stationModel)
+            self.filter_proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+            self.filter_proxy_model.setFilterKeyColumn(0)
+
+            self.ui.searchLineEdit.textChanged.connect(self.filter_proxy_model.setFilterRegularExpression)
+
+            self.ui.stationTableView.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+            self.ui.stationTableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+            self.ui.stationTableView.setModel(self.filter_proxy_model)
 
 
         
