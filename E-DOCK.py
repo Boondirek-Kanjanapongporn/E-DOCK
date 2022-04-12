758164aa6f135adc01f71f3c10249db098baecef
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2022-04-12 19:26:30 +0700

    [DELETE] comments

--- E-DOCK.py
+++ E-DOCK.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from LoginPage import Login_Page
 from SignupPage import Signup_Page
-from MainPage import Main_Page
 
 firebaseConfig={
     'apiKey': "AIzaSyBNtpVvn25ATKbJ0lg0ZdV-rhjJ1vkd5dA",
@@ -22,24 +21,15 @@ auth = firebase.auth()
 db = firebase.database()
 storage = firebase.storage()
 
-# data={'username': '', 'pin': '', 'notification': True, 'e-wallet': 0, 'cars':'', 'cards':'', 'history':''}
-# db.child('users').child("HaMQwP6aXESz3aHhnbH3TQAvflI3").set(data)
-
-# temp = db.child("users").child("HaMQwP6aXESz3aHhnbH3TQAvflI3").get()
-# print(temp.val()['notification'])
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     widget = QStackedWidget()
 
-    # login_page = Login_Page(auth, widget)
-    # signup_page = Signup_Page(auth, widget)
-    main_page = Main_Page(auth, widget)
+    login_page = Login_Page(db, auth, widget)
+    signup_page = Signup_Page(db, auth, widget)
 
-    # widget.addWidget(login_page)
-    # widget.addWidget(signup_page)
-    widget.addWidget(main_page)
+    widget.addWidget(login_page)
+    widget.addWidget(signup_page)
 
     widget.setFixedHeight(640)
     widget.setFixedWidth(360)
