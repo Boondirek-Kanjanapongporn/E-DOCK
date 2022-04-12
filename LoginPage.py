df93a3ba5e5d83f3731b68b0267976426e628816
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2022-04-12 19:27:16 +0700

    [CHANGE] stronger login

--- LoginPage.py
+++ LoginPage.py
@@ -3,15 +3,20 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from login import Ui_Form as Ui_Login
 from MainPage import Main_Page
+import pickle
 
 class Login_Page(QWidget):
-    def __init__(self, auth, widget):
+    def __init__(self, db, auth, widget):
         QWidget.__init__(self, None)
+        self.db = db
         self.auth = auth
         self.widget = widget
 
         self.ui = Ui_Login()
         self.ui.setupUi(self)
+
+        # Try autoLogin
+        self.autoLogin()
     
         self.ui.loginPushButton.clicked.connect(self.Login)
 
@@ -34,14 +39,16 @@ class Login_Page(QWidget):
                 self.ui.invalidpasswordLabel.setVisible(False)
 
                 try:
-                    self.auth.sign_in_with_email_and_password(email, password)
-                    print(f"Login account with email: {email} and password: {password} {checkbox}")
-
-                    main_page = Main_Page(self.auth, self.widget)
-                    self.widget.addWidget(main_page)
-                    self.widget.setCurrentIndex(self.widget.currentIndex() + 2)
+                    checkAuth = self.auth.sign_in_with_email_and_password(email, password)
+                    print("Login to account")
+                    # print(f"Login account with email: {email} and password: {password} {checkbox}")
                 except:
                     self.showAlert("Incorrect Email or Password")
+
+                if checkAuth:
+                    if checkbox:
+                        self.saveRememberMe(email, password)
+                    self.gotoMain_Page()
             else:
                 self.ui.invalidpasswordLabel.setVisible(True)
         else:
@@ -49,6 +56,28 @@ class Login_Page(QWidget):
     
     def gotoSignup_Page(self):
         self.widget.setCurrentIndex(self.widget.currentIndex() + 1)
+    
+    def gotoMain_Page(self):
+        main_page = Main_Page(self.db, self.auth, self.widget)
+        self.widget.addWidget(main_page)
+        self.widget.setCurrentIndex(self.widget.currentIndex() + 2)
+    
+    def saveRememberMe(self, email, password):
+        userData = {"email": email, "password": password}
+        with open('rememberUser.pkl','wb') as rememberUser:
+            pickle.dump(userData, rememberUser)
+    
+    def autoLogin(self):
+        try:
+            with open('rememberUser.pkl', 'rb') as rememberUser:
+                    userData = pickle.load(rememberUser)
+                    self.auth.sign_in_with_email_and_password(userData['email'], userData['password'])
+                    print("Login to account")
+                    # print(f"Login account with email: {userData['email']} and password: {userData['password']}")
+                    
+                    self.gotoMain_Page()
+        except:
+            print('No rememberUser file found')
 
     def showAlert(self, text):
         dialog = QDialog(self)
